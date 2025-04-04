fb1027b9d2922eee119cfae5ee61df307e09d785
Joacim Päivärinne <joacim@vaadin.com>
2025-04-04 13:43:38 +0300

    Update HomeView.java

--- src/main/java/com/example/application/view/HomeView.java
+++ src/main/java/com/example/application/view/HomeView.java
@@ -1,13 +1,9 @@
 package com.example.application.view;
 
 import com.example.application.view.component.ComponentView;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility;
 import com.vaadin.flow.theme.lumo.LumoUtility.Padding;
 
 @PageTitle("Home")
