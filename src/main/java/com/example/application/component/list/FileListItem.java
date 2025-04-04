4c32e1427b3c8194e08b1645753de4b5dff0a0da
Joacim Päivärinne <joacim@vaadin.com>
2025-04-04 12:09:09 +0300

    Update FileListItem.java

--- src/main/java/com/example/application/component/list/FileListItem.java
+++ src/main/java/com/example/application/component/list/FileListItem.java
@@ -4,7 +4,6 @@ import com.example.application.component.Layout;
 import com.example.application.component.Span;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Unit;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.theme.lumo.LumoUtility.*;
 
 public class FileListItem extends ListItem {
@@ -23,18 +22,20 @@ public class FileListItem extends ListItem {
     }
 
     private Component createFileIcon(String fileName) {
-        Div corner = new Div();
-        corner.addClassNames(Background.CONTRAST_50, Display.FLEX, Position.ABSOLUTE, Position.End.NONE,
-                Position.Top.NONE);
+        Layout corner = new Layout();
+        corner.addClassNames(Background.CONTRAST_50, Position.End.NONE, Position.Top.NONE);
         corner.setHeight(30, Unit.PERCENTAGE);
+        corner.setPosition(Layout.Position.ABSOLUTE);
         corner.setWidth(30, Unit.PERCENTAGE);
 
-        Div fileIcon = new Div(corner, new Span(fileName.substring(fileName.lastIndexOf(".") + 1)));
-        fileIcon.addClassNames(AlignItems.END, Background.PRIMARY, BorderRadius.SMALL, Display.FLEX,
-                FontSize.XXSMALL, FontWeight.MEDIUM, JustifyContent.CENTER, Padding.XSMALL, Position.RELATIVE,
-                TextColor.PRIMARY_CONTRAST);
+        Layout fileIcon = new Layout(corner, new Span(fileName.substring(fileName.lastIndexOf(".") + 1)));
+        fileIcon.addClassNames(Background.PRIMARY, BorderRadius.SMALL, FontSize.XXSMALL, FontWeight.MEDIUM,
+                Padding.XSMALL, TextColor.PRIMARY_CONTRAST);
         fileIcon.getStyle().set("clip-path", "polygon(0% 0%, 70% 0%, 100% 30%, 100% 100%, 0% 100%)");
+        fileIcon.setAlignItems(Layout.AlignItems.END);
         fileIcon.setHeight(1.75f, Unit.REM);
+        fileIcon.setJustifyContent(Layout.JustifyContent.CENTER);
+        fileIcon.setPosition(Layout.Position.RELATIVE);
         fileIcon.setWidth(1.25f, Unit.REM);
         return fileIcon;
     }
